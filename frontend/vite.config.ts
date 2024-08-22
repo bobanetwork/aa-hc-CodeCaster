3ecde992e4f39038b31e2aa13c3783512fa49578
wsdt <kevin.riedl@wavect.io>
2024-08-22 14:10:14 +0200

    fix: Buffer not exported

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+const inject = require('@rollup/plugin-inject')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
     },
   },
   server: {
