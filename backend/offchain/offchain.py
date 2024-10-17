ff82243279120f01fb894a73c70a0c811567d4b5
wsdt <kevin.riedl.privat@gmail.com>
2024-10-17 17:11:35 +0200

    fix: py sdk

--- backend/offchain/offchain.py
+++ backend/offchain/offchain.py
@@ -1,27 +1,25 @@
 from web3 import Web3
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer, SimpleJSONRPCRequestHandler
-from text2call import offchain_text2multi
+from server_action import offchain_text2multi
+from hybrid_compute_sdk import HybridComputeSDK
 import logging
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def selector(name):
-    nameHash = Web3.to_hex(Web3.keccak(text=name))
-    return nameHash[2:10]
-
-class RequestHandler(SimpleJSONRPCRequestHandler):
-    rpc_paths = ('/', '/hc')
-
 def offchain_hello(ver, sk, src_addr, src_nonce, oo_nonce, payload, *args):
     return { "success":True, "response":payload, "signature":"0x" }
 
 def server_loop():
-    server = SimpleJSONRPCServer(('0.0.0.0', 1234), requestHandler=RequestHandler)
-    server.register_function(offchain_hello, "hello")
-    server.register_function(offchain_text2multi, selector("text2multi(string)"))
-    print("Serving ")
-    print("PORT => {}".format(1234))
-    server.serve_forever()
+    # new sdk instance
+    sdk = HybridComputeSDK()
+    # prepare the server
+    sdk.create_json_rpc_server_instance()
+    # add a custom server action
+    sdk.add_server_action("text2multi(string)", offchain_text2multi)
+    # add another server action
+    sdk.add_server_action("hello", offchain_hello)
+    # start server
+    sdk.serve_forever()
 
-server_loop()  # Run until killed
+server_loop()
