3d7886aa187f2bf7feddccd4f4e9b580a9c9e219
wsdt <kevin.riedl.privat@gmail.com>
2025-07-10 12:25:47 +0200

    fix: env vals

--- frontend/src/config/snap.ts
+++ frontend/src/config/snap.ts
@@ -1,16 +1,7 @@
-
-/**
- * The snap origin to use.
- * Will default to the local hosted snap if no value is provided in environment.
- */
-export const defaultSnapOrigin = import.meta.env.VITE_SNAP_ORIGIN ?? 'npm:@bobanetwork/snap-account-abstraction-keyring-hc'
-
-
-/**
- * Version of snap installed so have to give release on each new version.
- * - can use to show the button to user to update snaps.
- */
-export const snapPackageVersion = import.meta.env.VITE_SNAP_VERSION ?? "1.1.16";
-
-
+export const defaultSnapOrigin = import.meta.env.VITE_SNAP_ORIGIN
+export const snapPackageVersion = import.meta.env.VITE_SNAP_VERSION;
 export const CUSTOM_CONTRACT = import.meta.env.VITE_SMART_CONTRACT;
+
+console.log(defaultSnapOrigin);
+console.log(snapPackageVersion);
+console.log(CUSTOM_CONTRACT);
